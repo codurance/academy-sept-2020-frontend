8c82b9dfbed4fa76ea6b7c045d28ac28a2eed3d3
martin-garcia-blanco <programadopormartin@gmail.com>
2020-11-26 21:16:55 +0100

    Fix error on an import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Routes/Home/Home';
+import Home from './routes/Home/Home';
 import './styles/global.scss';
 
 const App = () => {
