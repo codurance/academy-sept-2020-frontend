2707b0fb71f781dcf616b9c28cdd27aa937940a5
martin-garcia-blanco <programadopormartin@gmail.com>
2020-12-01 16:27:38 +0100

    Create apiCall util to facilitate the api requests

---
+++ src/utils/apiCall.js
@@ -0,0 +1,59 @@
+export const processOptions = (options) => {
+  const token = localStorage.getItem('authToken');
+  if (!options) return {};
+  let opts = { ...options };
+
+  if (options.auth && token) {
+    opts = {
+      ...opts,
+      headers: { ...opts.headers, Authorization: `Bearer ${token}` },
+    };
+  }
+  if (opts.method && opts.method.toUpperCase() !== 'GET') {
+    opts = {
+      ...opts,
+      headers: {
+        ...opts.headers,
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    };
+  }
+  if (opts.body) {
+    opts = {
+      ...opts,
+      body: JSON.stringify(opts.body),
+    };
+  }
+  opts = { ...opts, method: opts.method.toUpperCase() };
+  delete opts.auth;
+  return opts;
+};
+
+export const apiCall = async (url, options) => {
+  const response = await fetch(url, options);
+  let error = undefined;
+  const data = await response.json();
+
+  if (response.status.toString().startsWith('5')) {
+    console.log('SERVER ERROR', data.message);
+    error = data.message;
+  }
+  if (response.status.toString() === '403') {
+    console.log('FORBIDDEN', data.message);
+    error = data.message;
+  }
+  if (response.status.toString() === '400') {
+    console.log('BAD REQUEST', data.message);
+    error = data.message;
+  }
+  if (response.status.toString() === '401') {
+    console.log('UNAUTHORIZED', data.message);
+    error = data.message;
+  }
+  if (response.status.toString() === '404') {
+    console.log('NOT FOUND', data.message);
+    error = data.message;
+  }
+
+  return { error, data };
+};
---
+++ src/utils/apiCall.spec.js
@@ -0,0 +1,42 @@
+const { processOptions } = require('./apiCall');
+
+const tokenStub = 'stub';
+localStorage.setItem('authToken', tokenStub);
+
+describe('processOptions should', () => {
+  test('return an empty object given an empty, null or undefined parameter', () => {
+    expect(processOptions()).toEqual({});
+    expect(processOptions(null)).toEqual({});
+    expect(processOptions(undefined)).toEqual({});
+  });
+  test('return an object with Authorization header and GET method', () => {
+    expect(processOptions({ method: 'get', auth: true })).toEqual({
+      method: 'GET',
+      headers: { Authorization: 'Bearer stub' },
+    });
+  });
+  test('return an object with Authorization and Content-Type headers and POST method', () => {
+    expect(processOptions({ method: 'post', auth: true })).toEqual({
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer stub',
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    });
+  });
+  test('return an object with Authorization and Content-Type headers and POST method and a body', () => {
+    const bodyStub = { id: '123Stub' };
+    const bodyStubStringified = JSON.stringify(bodyStub);
+
+    expect(
+      processOptions({ method: 'post', auth: true, body: bodyStub })
+    ).toEqual({
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer stub',
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: bodyStubStringified,
+    });
+  });
+});
