2c8b76c10f37c692277309c4f13a6383723c4e28
martin-garcia-blanco <programadopormartin@gmail.com>
2020-12-01 14:14:46 +0100

    Disable automatic logIn

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { useGoogleLogin } from 'react-google-login';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
 
 const googleOAuthId = process.env.REACT_APP_GOOGLE_OAUTH_ID;
 
-function Login() {
+function Login({ onLoggin }) {
   const onSuccess = (res) => {
     console.log(res.profileObj);
     localStorage.setItem('authToken', res.googleId);
+    onLoggin(res);
   };
   const onFailure = (res) => {
     console.log(res);
@@ -16,10 +18,14 @@ function Login() {
     onSuccess,
     onFailure,
     clientId: googleOAuthId,
-    isSignedIn: true,
+    isSignedIn: false,
     accessType: 'offline',
   });
 
   return <Button callback={signIn} label={'Sign In'} variant={'big'} />;
 }
 export default Login;
+
+Login.propTypes = {
+  onLoggin: PropTypes.func,
+};
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import Login from '../../components/Login/Login';
 import Wrapper from '../../components/Wrapper/Wrapper';
 
 const Home = () => {
+  const [user, setUser] = useState(false);
+
+  const handleLoggin = (loggedUser) => {
+    setUser(true);
+  };
+
   return (
     <div>
       <Header />
       <Wrapper>
-        <Login />
+        {user ? <h1>Logged In </h1> : <Login onLoggin={handleLoggin} />}
       </Wrapper>
     </div>
   );
