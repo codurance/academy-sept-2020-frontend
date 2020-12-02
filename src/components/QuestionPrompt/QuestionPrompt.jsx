7a461b73ce98643b98f148ca9ae991c4a423eaae
martin-garcia-blanco <programadopormartin@gmail.com>
2020-12-02 11:29:00 +0100

    Implement toast and survey request

--- src/components/QuestionPrompt/QuestionPrompt.jsx
+++ src/components/QuestionPrompt/QuestionPrompt.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import './styles.scss';
+import React, { Fragment, useEffect, useState } from 'react';
+import { apiCall } from '../../utils/apiCall';
 import Button from '../Button/Button';
 import Tile from '../Tile/Tile';
-import { apiCall } from '../../utils/apiCall';
+import Toast from '../Toast/Toast';
+import './styles.scss';
 
 const QuestionPrompt = () => {
   const [textArea, setTextArea] = useState('');
   const [disableSubmission, setdisableSubmission] = useState(true);
+  const [toastTextArea, setToastTextArea] = useState('');
+  const [toastTitle, setToastTitle] = useState('');
+  const [toastHidden, setToastHidden] = useState(true);
 
   useEffect(() => {
     setdisableSubmission(textArea === '');
@@ -15,7 +19,7 @@ const QuestionPrompt = () => {
   function submit() {
     const email = localStorage.getItem('email');
     if (email) {
-      apiCall(
+      const { error } = apiCall(
         'http://all-aboard-api-dev.eu-west-2.elasticbeanstalk.com/survey',
         {
           method: 'POST',
@@ -23,30 +27,49 @@ const QuestionPrompt = () => {
           body: { email: email, preference: textArea },
         }
       );
+      setToastHidden(false);
+      setToastTitle(
+        error
+          ? 'Oh No! (but it works on my machine)'
+          : "You've been most helpful! Thank you!"
+      );
+      setToastTextArea(
+        error
+          ? 'Things happened, that weren’t supposed to happen. Please drop us a line on #allaboard slack channel, to let us know you’ve encountered an issue.'
+          : "These aren't the doids we're looking for. You can go about your business. Move along, move along."
+      );
     }
   }
 
   return (
-    <Tile
-      title={'How do you organise and make use of your learning resources?'}
-      textArea={
-        <textarea
-          defaultValue={textArea}
-          onChange={(event) => {
-            setTextArea(event.target.value);
-          }}
-          placeholder={'Answer here...'}
-        ></textarea>
-      }
-      button={
-        <Button
-          isDisabled={disableSubmission}
-          label={'Submit'}
-          variant={'big'}
-          callback={submit}
-        />
-      }
-    />
+    <Fragment>
+      <Tile
+        title={'How do you organise and make use of your learning resources?'}
+        textArea={
+          <textarea
+            defaultValue={textArea}
+            onChange={(event) => {
+              setTextArea(event.target.value);
+            }}
+            placeholder={'Answer here...'}
+          ></textarea>
+        }
+        button={
+          <Button
+            isDisabled={disableSubmission}
+            label={'Submit'}
+            variant={'big'}
+            callback={submit}
+          />
+        }
+      />
+
+      <Toast
+        textArea={toastTextArea}
+        title={toastTitle}
+        isHidden={toastHidden}
+      />
+    </Fragment>
   );
 };
 
---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import './styles.scss';
+import PropTypes from 'prop-types';
+
+function getVariant(variant) {
+  if (variant == 'positive') {
+    return 'toast--positive';
+  } else if (variant == 'negative') return 'toast--negative';
+  return 'toast--neutral';
+}
+
+function fadeOut(isHidden) {
+  return isHidden ? 'toast--invisible' : 'toast--visible';
+}
+const Toast = ({ variant = 'neutral', title, textArea, isHidden }) => {
+  console.log(title);
+  return (
+    <Fragment>
+      <article
+        hidden={isHidden}
+        className={`toast ${getVariant(variant)} ${fadeOut(isHidden)} `}
+      >
+        <h3>{title}</h3>
+        <div className={'toast__content'}>{textArea}</div>
+      </article>
+    </Fragment>
+  );
+};
+
+export default Toast;
+
+Toast.propTypes = {
+  variant: PropTypes.oneOf(['positive', 'neutral', 'negative']),
+  textArea: PropTypes.string,
+  title: PropTypes.string,
+  isHidden: PropTypes.bool,
+};
