18c6d194051f3f28984113fdd78b2a7761c304c5
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-08 16:10:25 +0100

    Adds initial Editor view
    - Renders title and description textareas
    - Handles textareas changes

--- src/routes/Editor/Editor.jsx
+++ src/routes/Editor/Editor.jsx
@@ -1,10 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Button from '../../components/Button/Button';
 
 function Editor() {
+  const [learningPath, setLearningPath] = useState({
+    title: null,
+    description: null,
+  });
+
+  const setNewLearningPath = (event, fieldName) => {
+    const newLearningPath = { ...learningPath };
+    newLearningPath[fieldName] = event.target.value;
+    setLearningPath(newLearningPath);
+  };
+
   return (
     <Fragment>
       <Button label={'Publish'} callback={() => {}} />
+      <textarea
+        aria-label="learning-path-title"
+        onChange={(event) => setNewLearningPath(event, 'title')}
+      />
+      <textarea
+        aria-label="learning-path-description"
+        onChange={(event) => setNewLearningPath(event, 'description')}
+      />
     </Fragment>
   );
 }
--- src/routes/Editor/editor.spec.js
+++ src/routes/Editor/editor.spec.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Editor from "./Editor";
+import Editor from './Editor';
 
 describe('Editor should render with title and description inputs and publish button', function () {
   test('should render publish button', () => {
     const { queryByText } = render(<Editor />);
     expect(queryByText('Publish')).toBeInTheDocument();
+  });
+
+  test('should render title and description input fields', () => {
+    const { queryByLabelText } = render(<Editor />);
+    expect(queryByLabelText('learning-path-title')).toBeInTheDocument();
+    expect(queryByLabelText('learning-path-description')).toBeInTheDocument();
+  });
+
+  test('should foo and bar', () => {
+    const { queryByLabelText, st } = render(<Editor />);
+    let element = queryByLabelText('learning-path-title');
+    expect(element).toBeInTheDocument();
   });
 });
