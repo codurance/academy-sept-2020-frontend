afc8791146ee7f4c96104769a1fe6fe783fcfbae
Fabio D'Amico <fabio.damico@codurance.com>
2020-12-09 12:51:36 +0000

    🔥 Removing empty lines

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,14 @@ const Header = ({ children }) => {
   return (
     <nav className={'header'}>
       <div className={'header__content'}>
-          <div className={'header__content--main'}>
-
-
-
-            <img className={'header__logo'} src={ReactLogo} alt="Codurance Logo" />
-            <h1>All Aboard</h1>
-          </div>
+        <div className={'header__content--main'}>
+          <img
+            className={'header__logo'}
+            src={ReactLogo}
+            alt="Codurance Logo"
+          />
+          <h1>All Aboard</h1>
+        </div>
         {children}
       </div>
     </nav>
