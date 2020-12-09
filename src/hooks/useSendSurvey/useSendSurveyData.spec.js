5b29384c10204de05761169ce1e2326db797995c
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-09 13:04:03 +0100

    Adapts unit test
    - Mocks inner hook

--- src/hooks/useSendSurvey/useSendSurveyData.spec.js
+++ src/hooks/useSendSurvey/useSendSurveyData.spec.js
@@ -1,11 +1,29 @@
-const { serverMock } = require('../../utils/mockServerResponse');
-import SendSurveyData from './useSendSurveyData';
+import useAuthenticatedApiCall from '../useAuthenticatedApiCall/useAuthenticatedApiCall';
 
-describe.skip('sendSurveyData should', () => {
+const { serverMock } = require('../../utils/mockServerResponse');
+import useSendSurveyData from './useSendSurveyData';
+
+jest.mock('../useAuthenticatedApiCall/useAuthenticatedApiCall');
+
+function mockSuccessfulApiCall(expectedData) {
+  useAuthenticatedApiCall.mockImplementationOnce(() => {
+    return function authenticatedApiCall() {
+      return {
+        error: undefined,
+      };
+    };
+  });
+}
+
+describe('sendSurveyData should', () => {
   test('send the information to the backend', async () => {
+    mockSuccessfulApiCall();
     serverMock();
     const body = { email: 'email@mail.com', preference: 'message' };
-    const error = await SendSurveyData(body);
+    const sendSurveyData = useSendSurveyData();
+
+    const error = await sendSurveyData(body);
+
     expect(error).toBe(undefined);
   });
 });
