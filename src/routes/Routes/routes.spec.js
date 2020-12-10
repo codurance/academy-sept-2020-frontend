33f8680548b537406bac19000993c6455af4a9f8
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-10 14:21:41 +0100

    Covers both Router use cases
    - User authenticated
    - User unauthenticated

--- src/routes/Routes/routes.spec.js
+++ src/routes/Routes/routes.spec.js
@@ -3,20 +3,45 @@ import { render } from '@testing-library/react';
 import Routes from './Routes';
 import Login from '../../components/Login/Login';
 import { useGoogleAuth } from '../../components/Login/GoogleAuthProvider';
+import QuestionPrompt from '../../components/QuestionPrompt/QuestionPrompt';
 
 jest.mock('../../components/Login/Login');
+jest.mock('../../components/QuestionPrompt/QuestionPrompt');
 jest.mock('../../components/Login/GoogleAuthProvider');
+
+function mockUnauthenticatedUser() {
+  useGoogleAuth.mockImplementation(() => {
+    return {
+      isSignedIn: false,
+    };
+  });
+}
+
+function mockAuthenticatedUser() {
+  useGoogleAuth.mockImplementation(() => {
+    return {
+      isSignedIn: true,
+    };
+  });
+}
 
 describe('Route component', () => {
   it('should not render home (survey view as of today) if user is unauthenticated', () => {
     Login.mockImplementationOnce(() => <>Sign In</>);
-    useGoogleAuth.mockImplementation(() => {
-      return {
-        isSignedIn: false,
-      };
-    });
+    mockUnauthenticatedUser();
+
     const { getByText } = render(<Routes />);
 
     expect(getByText('Sign In')).toBeInTheDocument();
+  });
+
+  it('should render home (survey view as of today) if user is authenticated', () => {
+    Login.mockImplementationOnce(() => <>Sign In</>);
+    QuestionPrompt.mockImplementationOnce(() => <>Survey</>);
+    mockAuthenticatedUser();
+
+    const { getByText } = render(<Routes />);
+
+    expect(getByText('Survey')).toBeInTheDocument();
   });
 });
