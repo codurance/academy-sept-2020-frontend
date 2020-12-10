9c89d9734d26163fadaf9c79039bada7db1fc594
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-10 13:58:02 +0100

    Removes Home component
    - As authorization logic is now in Routes component.

--- src/routes/Routes/Routes.jsx
+++ src/routes/Routes/Routes.jsx
@@ -1,10 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../Home/Home';
 import LearningPaths from '../LearningPaths/LearningPaths';
 import Editor from '../Editor/Editor';
 import { useGoogleAuth } from '../../components/Login/GoogleAuthProvider';
 import Login from '../../components/Login/Login';
+import QuestionPrompt from '../../components/QuestionPrompt/QuestionPrompt';
 
 export default function Routes() {
   const { isSignedIn } = useGoogleAuth();
@@ -13,10 +13,10 @@ export default function Routes() {
       <Router>
         {isSignedIn ? (
           <Switch>
-            <Route exact path="/survey" component={Home}/>
-            <Route exact path="/learningpaths" component={LearningPaths}/>
-            <Route exact path="/editor" component={Editor}/>
-            <Route path="/" component={Home}/>
+            <Route exact path="/survey" component={QuestionPrompt} />
+            <Route exact path="/learningpaths" component={LearningPaths} />
+            <Route exact path="/editor" component={Editor} />
+            <Route path="/" component={QuestionPrompt} />
           </Switch>
         ) : (
           <Login />
