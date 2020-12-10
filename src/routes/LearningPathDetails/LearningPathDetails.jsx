97a9872a3b5ee22f82c643045129c4a3c99b56f3
martin-garcia-blanco <programadopormartin@gmail.com>
2020-12-10 16:27:52 +0100

    Add typing for learningPAthDetail component

--- src/routes/LearningPathDetails/LearningPathDetails.jsx
+++ src/routes/LearningPathDetails/LearningPathDetails.jsx
@@ -4,9 +4,10 @@ import Header from '../../components/Header/Header';
 import Wrapper from '../../components/Wrapper/Wrapper';
 import useGetLearningPathDetails from '../../hooks/useGetLearninPaths/useGetLearningPathDetails';
 import { useGoogleAuth } from '../../components/Login/GoogleAuthProvider';
+import PropTypes from 'prop-types';
 
-const LearningPathDetails = function (props) {
-  const learningPathId = props.match.params.id;
+const LearningPathDetails = function ({ match }) {
+  const learningPathId = match.params.id;
 
   const [data, setData] = useState([]);
   const [error, setError] = useState();
@@ -26,19 +27,19 @@ const LearningPathDetails = function (props) {
     fetchData();
   }, [isSignedIn]);
 
-    const listTopics = function(topics){
-      if(topics === undefined ){
-          return;
-      }
-      return topics.map((topic, index) => {
-        return(
-          <Fragment key={index}>
-            <h3>{topic.name}</h3>
-            <p>{topic.description}</p>
-          </Fragment>
-        );
-      });
-    };
+  const listTopics = function (topics) {
+    if (topics === undefined) {
+      return;
+    }
+    return topics.map((topic, index) => {
+      return (
+        <Fragment key={index}>
+          <h3>{topic.name}</h3>
+          <p>{topic.description}</p>
+        </Fragment>
+      );
+    });
+  };
 
   return (
     <Fragment>
@@ -47,10 +48,14 @@ const LearningPathDetails = function (props) {
         <h3>Details</h3>
         <p>{data.name}</p>
         <p>{data.description}</p>
-          {listTopics(data.topics)}
+        {listTopics(data.topics)}
       </Wrapper>
     </Fragment>
   );
 };
 
 export default LearningPathDetails;
+
+LearningPathDetails.propTypes = {
+  match: PropTypes.object,
+};
