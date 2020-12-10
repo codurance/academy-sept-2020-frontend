157d919bc7be828a4e5f374518f77330b4a02b89
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-10 13:30:38 +0100

    Extracts to delegate
    - Routes extracted from App to Routes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './routes/Home/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/global.scss';
 import { GoogleAuthProvider } from './components/Login/GoogleAuthProvider';
-import LearningPaths from './routes/LearningPaths/LearningPaths';
-import Editor from './routes/Editor/Editor';
+import Routes from './routes/Routes';
 
 const App = () => {
   return (
     <React.StrictMode>
       <Router>
         <GoogleAuthProvider>
-          <Switch>
-            <Route exact path="/survey" component={Home} />
-            <Route exact path="/learningpaths" component={LearningPaths} />
-            <Route exact path="/editor" component={Editor} />
-            <Route path="/" component={Home} />
-          </Switch>
+          <Routes />
         </GoogleAuthProvider>
       </Router>
     </React.StrictMode>
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Home/Home';
+import LearningPaths from './LearningPaths/LearningPaths';
+import Editor from './Editor/Editor';
+
+export default function Routes() {
+  return (
+    <Fragment>
+      <Switch>
+        <Route exact path="/survey" component={Home} />
+        <Route exact path="/learningpaths" component={LearningPaths} />
+        <Route exact path="/editor" component={Editor} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Fragment>
+  );
+}
