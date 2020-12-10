98d3e5b757ffeccbe26ff11eba21d718d9755349
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-10 13:29:44 +0100

    Adds another App test

--- src/app.spec.js
+++ src/app.spec.js
@@ -2,19 +2,47 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 import { useGoogleLogin } from 'react-use-googlelogin';
+import Home from './routes/Home/Home';
+import QuestionPrompt from './components/QuestionPrompt/QuestionPrompt';
 
 jest.mock('react-use-googlelogin');
+jest.mock('./components/QuestionPrompt/QuestionPrompt');
+
+function mockUnauthorizedUser() {
+  useGoogleLogin.mockImplementation(() => {
+    return {
+      googleUser: undefined,
+      grantOfflineAccess: () => {},
+      isSignedIn: false,
+    };
+  });
+}
+
+function mockAuthorizedUser() {
+  useGoogleLogin.mockImplementation(() => {
+    return {
+      googleUser: true,
+      grantOfflineAccess: () => {},
+      isSignedIn: true,
+    };
+  });
+}
 
 describe('App protects some routes if the user is not authenticated and redirects to login if so.', () => {
   it('should render the login button if user is unauthenticated', () => {
-    useGoogleLogin.mockImplementation(() => {
-      return {
-        googleUser: undefined,
-        grantOfflineAccess: () => {},
-        isSignedIn: false,
-      };
-    });
+    mockUnauthorizedUser();
+
     const { getByText } = render(<App />);
+
     expect(getByText('Sign In')).toBeInTheDocument();
+  });
+
+  it('should render the home (survey view as of today) if user is authenticated', () => {
+    mockAuthorizedUser();
+    QuestionPrompt.mockImplementation(() => <>Survey</>);
+
+    const { getByText } = render(<App />);
+
+    expect(getByText('Survey')).toBeInTheDocument();
   });
 });
