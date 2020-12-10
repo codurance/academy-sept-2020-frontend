5aa8df59ce6ff97dc0b4b24af445df3d189a5bd4
martin-garcia-blanco <programadopormartin@gmail.com>
2020-12-10 17:44:42 +0100

    ✨ Add a route for learningpath detail

--- src/routes/Routes/Routes.jsx
+++ src/routes/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import Editor from '../Editor/Editor';
 import { useGoogleAuth } from '../../components/Login/GoogleAuthProvider';
 import Login from '../../components/Login/Login';
 import QuestionPrompt from '../../components/QuestionPrompt/QuestionPrompt';
+import LearningPathDetails from '../LearningPathDetails/LearningPathDetails';
 
 export default function Routes() {
   const { isSignedIn } = useGoogleAuth();
@@ -13,6 +14,11 @@ export default function Routes() {
     <>
       <Switch>
         <Route exact path="/survey" component={QuestionPrompt} />
+        <Route
+          exact
+          path="/learningpaths/:id"
+          component={LearningPathDetails}
+        />
         <Route exact path="/learningpaths" component={LearningPaths} />
         <Route exact path="/editor" component={Editor} />
         <Route path="/" component={QuestionPrompt} />
