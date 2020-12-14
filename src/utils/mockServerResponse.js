c083e4f6cbc50f4921f275675e93000d7a172624
Martín García Blanco <programadopormartin@gmail.com>
2020-12-14 10:36:44 +0100

    ✨ Create a mock for /topic/id get endpoint

--- src/utils/mockServerResponse.js
+++ src/utils/mockServerResponse.js
@@ -32,6 +32,34 @@ export const serverMock = () => {
         ],
       }));
       this.post(`${BACKEND_API_URL}/survey`, () => {});
+      this.get(`${BACKEND_API_URL}/topic/1`, () => ({
+        id: 1,
+        name: 'topic',
+        description: 'description',
+        subtopics: [
+          {
+            name: 'subtopic 1',
+            id: 1,
+            resources: [
+              {
+                label: 'label',
+                url:
+                  'https://id.atlassian.com/login?continue=https%3A%2F%2Finterficie.atlassian.net%2Flogin%3FredirectCount%3D1%26dest-url%3D%252Fsecure%252FRapidBoard.jspa%253FrapidView%253D45%2526projectKey%253DIBS%2526selectedIssue%253DIBS-105%26application%3Djira&application=jira',
+              },
+            ],
+          },
+          {
+            name: 'subtopic 2',
+            id: 2,
+            resources: [
+              {
+                label: 'label 2',
+                url: 'https://www.nbamaniacs.com/',
+              },
+            ],
+          },
+        ],
+      }));
     },
   });
 };
