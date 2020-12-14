8a65dfdc8fab0a795e322c861771e6b23d40b418
Martín García Blanco <programadopormartin@gmail.com>
2020-12-14 10:41:55 +0100

    ✨ Fetch data from TopicDetails component

--- src/routes/TopicDetails/TopicDetails.jsx
+++ src/routes/TopicDetails/TopicDetails.jsx
@@ -1,10 +1,32 @@
-import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import React, { Fragment, useEffect, useState } from 'react';
+import Header from '../../components/Header/Header';
+import Wrapper from '../../components/Wrapper/Wrapper';
+import useGetTopicDetails from '../../hooks/useGetTopicDetail/useGetTopicDetails';
+import { serverMock } from '../../utils/mockServerResponse';
 
 const TopicDetails = ({ match }) => {
+  const [topic, setTopic] = useState();
+  const [error, setError] = useState();
+  const getTopicDetails = useGetTopicDetails();
+  const topicId = match.params.id;
+
+  const fetchData = async () => {
+    serverMock();
+
+    const { error, data } = await getTopicDetails(topicId);
+    setError(error);
+    setTopic(data);
+  };
+  console.log(topic);
+
+  useEffect(() => {
+    fetchData();
+  }, [topicId]);
   return (
     <Fragment>
-      <h2>Topic details</h2>
+      <Header />
+      <Wrapper>{topicId}</Wrapper>
     </Fragment>
   );
 };
