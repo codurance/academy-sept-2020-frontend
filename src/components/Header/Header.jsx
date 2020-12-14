f6e52ac7d7b2c039ea50d388f9fb8db5f5771a0f
Martín García Blanco <programadopormartin@gmail.com>
2020-12-14 16:39:35 +0100

    ✨ Header admits title and subtitle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import ReactLogo from '../../assets/icons/codurance-logo.svg';
 import './styles.scss';
 import PropTypes from 'prop-types';
 
-const Header = ({ children, title = 'All Aboard' }) => {
+const Header = ({ children, title = 'All Aboard', subtitle }) => {
   return (
     <nav className={'header'}>
       <div className={'header__content'}>
@@ -13,7 +13,9 @@ const Header = ({ children, title = 'All Aboard' }) => {
             src={ReactLogo}
             alt="Codurance Logo"
           />
-          <h1>{title}</h1>
+
+          <h1 className={'header__title'}>{title}</h1>
+          {subtitle && <h2 className={'header__subtitle'}>{subtitle}</h2>}
         </div>
         {children}
       </div>
@@ -26,4 +28,5 @@ export default Header;
 Header.propTypes = {
   children: PropTypes.element,
   title: PropTypes.string,
+  subtitle: PropTypes.string,
 };
