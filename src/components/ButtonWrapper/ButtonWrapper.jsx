3234fa5477c4784a26c0bc537a9030dd2ba6fd06
Martín García Blanco <programadopormartin@gmail.com>
2020-12-16 16:46:11 +0100

    ✨ Create a variant for ButtonWrapper

--- src/components/ButtonWrapper/ButtonWrapper.jsx
+++ src/components/ButtonWrapper/ButtonWrapper.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-const ButtonWrapper = ({ children }) => {
-  return <div className="button-wrapper">{children}</div>;
+const sizeSelector = (size) => {
+  return size === '100' ? 'button-wrapper--big' : '';
+};
+
+const ButtonWrapper = ({ children, size = '75' }) => {
+  return (
+    <div className={`button-wrapper ${sizeSelector(size)}`}>{children}</div>
+  );
 };
 
 export default ButtonWrapper;
 ButtonWrapper.propTypes = {
   children: PropTypes.node.isRequired,
+  size: PropTypes.oneOf(['75', '100']),
 };
