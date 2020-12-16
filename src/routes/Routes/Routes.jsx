bd49c31376737db732469413e3da09ac06ab7beb
Martín García Blanco <programadopormartin@gmail.com>
2020-12-16 15:37:48 +0100

    🔥 Removed unused component

--- src/routes/Routes/Routes.jsx
+++ src/routes/Routes/Routes.jsx
@@ -1,13 +1,12 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LearningPaths from '../LearningPaths/LearningPaths';
-import Editor from '../Editor/Editor';
 import { useGoogleAuth } from '../../components/Login/GoogleAuthProvider';
 import Login from '../../components/Login/Login';
 import QuestionPrompt from '../../components/QuestionPrompt/QuestionPrompt';
+import Editor from '../Editor/Editor';
 import LearningPathDetails from '../LearningPathDetails/LearningPathDetails';
+import LearningPaths from '../LearningPaths/LearningPaths';
 import TopicDetails from '../TopicDetails/TopicDetails';
-import Update from '../../routes/Editor/Update';
 import CreateTopic from '../TopicEditor/CreateTopic';
 
 export default function Routes() {
@@ -19,7 +18,6 @@ export default function Routes() {
         <Route exact path="/survey" component={QuestionPrompt} />
         <Route exact path="/learningpath/:id" component={LearningPathDetails} />
         <Route exact path="/editor/new-topic" component={CreateTopic} />
-        <Route exact path="/editor/:id" component={Update} />
         <Route exact path="/editor" component={Editor} />
         <Route
           exact
