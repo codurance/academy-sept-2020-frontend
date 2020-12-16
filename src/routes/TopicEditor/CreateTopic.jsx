0245ba5eb52a6d6237478bd665e542578faf3dbd
Martín García Blanco <programadopormartin@gmail.com>
2020-12-16 11:23:49 +0100

    ✨ Now the topic just redirects to editor on save

--- src/routes/TopicEditor/CreateTopic.jsx
+++ src/routes/TopicEditor/CreateTopic.jsx
@@ -1,16 +1,59 @@
-import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import React, { Fragment, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import 'styles.scss';
+import AddSubtopic from '../../components/AddSubtopic/AddSubtopic';
+import Button from '../../components/Buttons/Button/Button';
 import Header from '../../components/Header/Header';
 import Wrapper from '../../components/Wrapper/Wrapper';
 
-const CreateTopic = () => {
+const CreateTopic = ({ match }) => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [subtopics, setSubtopics] = useState([]);
+
+  const learningpathId = match.params.id;
+  const history = useHistory();
+
+  const onSaveTopic = async () => {
+    history.push(`/editor`);
+  };
+
   return (
     <Fragment>
-      <Header />
+      <Header title="Topic Editor" />
       <Wrapper>
-        <h3>Topic Creator</h3>
+        <div className="button-wrapper">
+          <Button label="Save" callback={onSaveTopic} />
+        </div>
+
+        <section className="editor__container">
+          <h3>Title</h3>
+          <textarea
+            className={'title'}
+            maxLength={500}
+            aria-label="learning-path-title"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <h3>Description</h3>
+          <textarea
+            aria-label="learning-path-description"
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+          />
+
+          {<AddSubtopic subtopics={subtopics} setSubtopics={setSubtopics} />}
+        </section>
       </Wrapper>
     </Fragment>
   );
 };
 
 export default CreateTopic;
+
+CreateTopic.propTypes = {
+  match: PropTypes.object,
+};
